c1025496fe6a9330bf74ae4c1867afbdd1b465c5
Nikolay Pavlin <afaneor@gmail.com>
2023-01-10 21:06:53 +0300

    feat: updated bot version

--- src/commands.py
+++ src/commands.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: CallbackContext) -> None:
     logger.info('get start message')
     if update.message:
-        update.message.reply_text('docker telegram boilerplate')
+        await update.message.reply_text('docker telegram boilerplate')
--- src/main.py
+++ src/main.py
@@ -1,27 +1,17 @@
-from telegram.ext import Updater
+from telegram.ext import ApplicationBuilder
 
-from conf.settings import BOT_TOKEN, JOB_TIME, logger
+from conf.settings import BOT_TOKEN, logger
 from handlers import start_handler
-from jobs import start_job
 
 
 def main():
-    updater = Updater(token=BOT_TOKEN)
-    dispatcher = updater.dispatcher
-    job_queue = updater.job_queue
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    dispatcher.add_handler(start_handler)
-
-    job_queue.run_repeating(start_job, JOB_TIME)
+    application.add_handler(start_handler)
 
     # Start the Bot
-    updater.start_polling()
+    application.run_polling()
     logger.info('bot started')
-
-    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
-    # SIGABRT. This should be used most of the time, since start_polling() is
-    # non-blocking and will stop the bot gracefully.
-    updater.idle()
 
 
 if __name__ == '__main__':
